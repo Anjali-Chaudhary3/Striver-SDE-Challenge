ff7dbe86b4723270e2c7fc4f77c1f5f18750a029
Anjali-Chaudhary3 <96697247+Anjali-Chaudhary3@users.noreply.github.com>
2023-08-01 11:39:47 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -6,26 +6,43 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    //arr1,arr2 : the arrays
-    // n, m: size of arrays
-    //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        //Your code here
-        //return vector with correct order of elements
         vector<int> ans;
         set<int> s;
-        for(int i=0;i<n;i++)
+        int j=0,i=0;
+        while(i<n && j<m)
         {
-            s.insert(arr1[i]);
+            if(arr1[i]<arr2[j] )
+            {
+                if(ans.size()==0 || ans.back()!=arr1[i])
+                ans.push_back(arr1[i]);
+                ++i;
+            }
+            else if(arr1[i]>arr2[j])
+            {
+                if(ans.size()==0 || ans.back()!=arr2[j])
+                ans.push_back(arr2[j]);
+                ++j;
+            }
+            else {
+                if(ans.size()==0 || ans.back()!=arr2[j])
+                  ans.push_back(arr2[j]);
+                  ++j;
+                  ++i;
+            }
         }
-         for(int j=0;j<m;j++)
+        while(i<n)
         {
-            s.insert(arr2[j]);
+             if(ans.size()==0 || ans.back()!=arr1[i])
+            ans.push_back(arr1[i]);
+                ++i;
         }
-        for(auto i:s)
+        while(j<m)
         {
-            ans.push_back(i);
+            if(ans.size()==0 || ans.back()!=arr2[j])
+            ans.push_back(arr2[j]);
+                ++j;
         }
         return ans;
     }
